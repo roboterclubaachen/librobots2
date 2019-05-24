fac6af25526591a9052c3b388aec7858dd3eb3fd
Christopher Durand <christopher.durand@rwth-aachen.de>
2019-05-24 18:39:44 +0200

    Add BLDC motor

---
+++ src/motor/bldc_motor_block_commutation.hpp
@@ -0,0 +1,60 @@
+/* bldc_motor_block_commutation.hpp
+ *
+ * Copyright (C) 2019 Christopher Durand
+ *
+ * TODO: license
+ */
+
+#ifndef LIBMOTOR_BLDC_MOTOR_BLOCK_COMMUTATION_HPP
+#define LIBMOTOR_BLDC_MOTOR_BLOCK_COMMUTATION_HPP
+
+#include <cstdint>
+
+#include "motor_base.hpp"
+
+namespace libmotor
+{
+
+/// Control of a BLDC motor with block commutation
+template<typename MotorBridge>
+class BldcMotorBlockCommutation final : public MotorBase<MotorBridge>
+{
+public:
+	BldcMotorBlockCommutation(uint_fast8_t commutationOffset);
+
+	/// Disables the motor on destruction
+	~BldcMotorBlockCommutation();
+
+	/// Set pwm value in range of -32767...32767
+	void setPwm(int16_t pwm) override;
+
+	void disable() override;
+
+	/// do commutation, can be called from an interrupt context
+	void update() override;
+
+	// These cannot be defaulted because the class has volatile member variables
+	BldcMotorBlockCommutation(const BldcMotorBlockCommutation&);
+	BldcMotorBlockCommutation& operator=(const BldcMotorBlockCommutation&);
+private:
+	void disableMotor();
+
+	enum class Mode
+	{
+		Pwm,
+		Brake,
+		Disabled
+	};
+
+	using MotorBase<MotorBridge>::ScaleFactor;
+
+	volatile uint_fast8_t commutationOffset_;
+	volatile bool reverse_ = false;
+	volatile Mode mode_ = Mode::Disabled;
+};
+
+}
+
+#include "bldc_motor_block_commutation_impl.hpp"
+
+#endif // LIBMOTOR_BLDC_MOTOR_BLOCK_COMMUTATION_HPP
---
+++ src/motor/bldc_motor_block_commutation_impl.hpp
@@ -0,0 +1,101 @@
+/* dc_motor_impl.hpp
+ *
+ * Copyright (C) 2019 Christopher Durand
+ *
+ * TODO: license
+ */
+
+#ifndef LIBMOTOR_BLDC_MOTOR_BLOCK_COMMUTATION_HPP
+#error "Do not use this file directly, include 'block_commutation.hpp' instead!"
+#endif
+
+#include "block_commutation.hpp"
+
+namespace libmotor
+{
+
+template<typename MotorBridge>
+BldcMotorBlockCommutation<MotorBridge>::BldcMotorBlockCommutation(uint_fast8_t commutationOffset)
+	: commutationOffset_{commutationOffset}
+{
+	disableMotor();
+}
+
+template<typename MotorBridge>
+BldcMotorBlockCommutation<MotorBridge>::~BldcMotorBlockCommutation()
+{
+	disableMotor();
+}
+
+template<typename MotorBridge>
+void BldcMotorBlockCommutation<MotorBridge>::setPwm(int16_t pwm)
+{
+	// limit negative pwm at -32767, 32768 is not representable with int16_t
+	if(pwm < -std::numeric_limits<int16_t>::max()) {
+		pwm = -std::numeric_limits<int16_t>::max();
+	}
+
+	if (pwm > 0) {
+		mode_ = Mode::Pwm;
+		reverse_ = false;
+	} else if (pwm < 0) {
+		mode_ = Mode::Pwm;
+		reverse_ = true;
+		pwm = -pwm;
+	} else /* (pwm == 0) */ {
+		mode_ = Mode::Brake;
+	}
+
+	MotorBridge::setCompareValue(pwm / ScaleFactor);
+	update();
+}
+
+template<typename MotorBridge>
+void BldcMotorBlockCommutation<MotorBridge>::update()
+{
+	using Commutation = BlockCommutation<typename MotorBridge::HallPort>;
+
+	BridgeConfig config = {};
+	if(mode_ == Mode::Pwm) {
+		config = Commutation::doCommutation(commutationOffset_, reverse_);
+	} else if(mode_ == Mode::Brake) {
+		config = Commutation::motorBrake();
+	} else /* disable motor */ {
+		config = Commutation::disable();
+	}
+
+	MotorBridge::configure(config);
+}
+
+template<typename MotorBridge>
+void BldcMotorBlockCommutation<MotorBridge>::disable()
+{
+	disableMotor();
+}
+
+template<typename MotorBridge>
+void BldcMotorBlockCommutation<MotorBridge>::disableMotor()
+{
+	mode_ = Mode::Disabled;
+	update();
+}
+
+// copy/move-constructors, assignment operators. Cannot be defaulted due to volatile members.
+
+template<typename MotorBridge>
+BldcMotorBlockCommutation<MotorBridge>::BldcMotorBlockCommutation(const BldcMotorBlockCommutation& other)
+	: commutationOffset_{other.commutationOffset_}, reverse_{other.reverse_}, mode_{other.mode_}
+{
+}
+
+template<typename MotorBridge>
+BldcMotorBlockCommutation<MotorBridge>&
+BldcMotorBlockCommutation<MotorBridge>::operator=(const BldcMotorBlockCommutation& other)
+{
+	commutationOffset_ = other.commutationOffset_;
+	reverse_ = other.reverse_;
+	mode_ = other.mode_;
+	return *this;
+}
+
+}
